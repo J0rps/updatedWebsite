28dc349c034edd72105237e95afb127ec8c734c7
J0rps <wruizfn@gmail.com>
2023-08-07 05:09:08 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuburg = document.querySelector(".menu-burg");
+const navLinks = document.querySelector(".nav-links");
+menuburg.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile-menu')
+})
